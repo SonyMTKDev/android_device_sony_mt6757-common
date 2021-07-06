db5794717c75127ec8ffdcee4a84cc540e61bdc7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-06 05:37:44 +0200

    mt6757-common: Build netutils-wrapper-1.0
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I3477770a264557e703d026b7ab29e35031478b21

--- mt6757.mk
+++ mt6757.mk
@@ -141,6 +141,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.mtkrc.path=/vendor/etc/init/hw/
 
+# Net
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0 \
