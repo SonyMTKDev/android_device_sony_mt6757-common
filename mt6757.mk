b49ba84dc1fae8042c60d60409543daa86aa9445
Michael Scott <michael.scott@linaro.org>
2021-07-06 18:27:50 +0200

    mt6757-common: add libion dependency

--- mt6757.mk
+++ mt6757.mk
@@ -100,7 +100,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
-    libGLES_android
+    libGLES_android \
+    libion
 
 # HIDL
 PRODUCT_PACKAGES += \
